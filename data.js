c3b98d6f33f50f647d16a60155976f7783ef37f8
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-07-11 22:29:26 +0100

    initial commit.

---
+++ data.js
@@ -0,0 +1,32 @@
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        id: 0,
+        img: {
+            src: "/images/pizza.png",
+            alt: "pizza"
+        }
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        id: 1,
+        img: {
+            src: "/images/burger.png",
+            alt: "burger"
+        }
+    },
+    {
+        name: "Soda",
+        ingredients: ["coke", "sprite", "fanta"],
+        price: 2,
+        id: 2,
+        img: {
+            src: "/images/soda.png",
+            alt: "soda"
+        }
+    }
+]
